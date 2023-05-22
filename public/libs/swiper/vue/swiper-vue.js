a4e942216f84c83369d5ddc19b1a0668c14c1f81
Janis <janis.paberzs18@gmail.com>
2023-05-22 09:53:05 +0300

    Initial commit

---
+++ public/libs/swiper/vue/swiper-vue.js
@@ -0,0 +1,24 @@
+/**
+<<<<<<< HEAD
+ * Swiper Vue 9.2.2
+=======
+ * Swiper Vue 9.2.3
+>>>>>>> 0265bc473c2d4fce1e2a91b366e20431e219b44c
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * https://swiperjs.com
+ *
+ * Copyright 2014-2023 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+<<<<<<< HEAD
+ * Released on: April 14, 2023
+=======
+ * Released on: April 17, 2023
+>>>>>>> 0265bc473c2d4fce1e2a91b366e20431e219b44c
+ */
+
+import { Swiper } from './swiper.js';
+import { SwiperSlide } from './swiper-slide.js';
+export { useSwiperSlide, useSwiper } from './context.js';
+export { Swiper, SwiperSlide };
